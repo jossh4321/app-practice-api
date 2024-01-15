301e49965f8540508e69d7da531f9df4e946b9f3
Josue Colombo <josuec4321@gmail.com>
2024-01-15 02:10:14 -0500

    first commit

---
+++ ProductApi/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProductApi.Models;
+using ProductApi.Service;
+
+namespace ProductApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ProductController : Controller
+    {
+        private readonly ProductService service;
+        
+        public ProductController()
+        {
+            service = new ProductService();
+        }
+
+        [HttpGet()]
+        public IActionResult GetAllProducts()
+        {
+            return Ok(service.GetAllProducts());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(string id)
+        {
+            return Ok(service.GetProduct(id));
+        }
+    }
+}
---
+++ ProductApi/Service/ProductService.cs
@@ -0,0 +1,59 @@
+﻿using ProductApi.Models;
+using System.Data.SqlClient;
+
+namespace ProductApi.Service
+{
+    public class ProductService
+    {
+
+        public SqlConnection GetConnection()
+        {
+            string connectionString = "Server=tcp:sqlserver01001.database.windows.net,1433;Initial Catalog=sqldbapp0100;Persist Security Info=False;User ID=sqlusr;Password=PR1M4V3R42023..;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            return new SqlConnection(connectionString);
+        }
+
+
+        public List<Product> GetAllProducts()
+        {
+            List<Product> productList = new List<Product>();
+            string query = "SELECT ProductId, ProductName, Quantity FROM Products";
+            SqlConnection conn = GetConnection();
+            conn.Open();
+            SqlCommand sqlCommand = new SqlCommand(query, conn);
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                productList.Add(new Product()
+                {
+                    ProductId = reader.GetString(0),
+                    ProductName = reader.GetString(1),
+                    Quantity = reader.GetInt32(2)
+                });
+            }
+            conn.Close();
+            return productList;
+        }
+
+        public Product GetProduct(string productId)
+        {
+            Product product = new Product();
+            string query = $"SELECT ProductId, ProductName, Quantity FROM Products WHERE ProductId = '{productId}'";
+            SqlConnection conn = GetConnection();
+            conn.Open();
+            SqlCommand sqlCommand = new SqlCommand(query, conn);
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                product = new Product()
+                {
+                    ProductId = reader.GetString(0),
+                    ProductName = reader.GetString(1),
+                    Quantity = reader.GetInt32(2)
+                };
+            }
+            conn.Close();
+            return product;
+        }
+
+    }
+}
